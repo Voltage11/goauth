e400eb1e2e6ac0c72e9c545e711f46d6963a3570
Vladimir Kukhlevskiy <voltage2002@yandex.ru>
2026-02-11 16:00:54 +0300

    Добавлен логгерр

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"goauth/internal/config"
+	"goauth/pkg/logger"
+	"strings"
 )
+
+const APP_NAME = "goauth"
 
 func main() {
 	// Загрузка конфигурации
 	cfg := config.GetConfig()
-	fmt.Println(cfg)
+
+	// Логгер
+	appLogger := logger.New(logger.AppLoggerLevelDebug, APP_NAME)
+
+	// До запуска сервера установим уровень логирования из настроек
+	appLogger.SetLevel(getLogLevelFromStr(cfg.Logger.Level))
+}
+
+func getLogLevelFromStr(logLevelStr string) logger.AppLoggerLevel {
+	switch strings.ToLower(logLevelStr) {
+	case "debug":
+		return logger.AppLoggerLevelDebug
+	case "info":
+		return logger.AppLoggerLevelInfo
+	case "warn":
+		return logger.AppLoggerLevelWarn
+	case "error":
+		return logger.AppLoggerLevelError
+	default:
+		return logger.AppLoggerLevelInfo
+	}
 }
